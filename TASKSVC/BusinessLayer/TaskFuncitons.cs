ed619ddfb523ec95a9cad0b3f0397f4ab56bf307
t-jlcahiyang <134570831+t-jlcahiyang@users.noreply.github.com>
2023-05-25 10:53:38 +0800

    Add files via upload

---
+++ TASKSVC/BusinessLayer/TaskFuncitons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TASKSVC.BusinessLayer
+{
+    public class TaskFuncitons
+    {
+
+        string Conn = ConfigurationManager.AppSettings["TASKConnection"];
+
+
+
+        public List 
+    }
+}
